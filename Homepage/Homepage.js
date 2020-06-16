c13c3301e0c7456d65a8b6972a92b644c31e3ec2
tanita plaengprawat <tanitaplaeng@gmail.com>
2020-06-16 12:01:49 -0700

    updated images and worked on modal popup

--- Homepage/Homepage.js
+++ Homepage/Homepage.js
@@ -22,3 +22,36 @@ function down(min) {
         document.getElementById("myNumber").value = min;
     }
 }
+
+// modal 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function openModal(event) { 
+    modal.style.display = "block";
+}
+
+const menuItems = document.getElementsByClassName('item');
+// [htmlElement, htmlElement, ...]
+console.log(menuItems);
+
+for (const item of menuItems) {
+    item.addEventListener('click', (e) => {
+        openModal(e);
+    })
+}
