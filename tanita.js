0eaa8a0aca301fdd5bf5ea99e77a37d1f2690ebd
armst209 <armst209@gmail.com>
2020-06-17 15:10:16 -0400

    revised code for checkout

--- tanita.js
+++
@@ -1,8 +0,0 @@
-const navBar = document.getElementById('nav-bar');
-const popupMenu = document.getElementById('custom-popup-menu');
-
-function hideNav() { 
-    // click on item class (line 77 in homepage.html)
-    // hides navBar (logo and home/product links)
-    // popup menu shows
-}
