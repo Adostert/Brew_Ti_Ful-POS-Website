7b5a2c2f88eca5e4233b0d4c9e36b563fbb4b6e6
tanita plaengprawat <tanitaplaeng@gmail.com>
2020-06-16 19:58:20 -0700

    merged popup html/css/js

--- Homepage/Homepage.js
+++ Homepage/Homepage.js
@@ -1,13 +1,13 @@
 // open and close popup
-let popupDiv = document.getElementById("popup-bg");
+// let popupDiv = document.getElementById("popup-bg");
 
-function openPopupMenu() {
-    popupDiv.style.display = "block";
-}
+// function openPopupMenu() {
+//     popupDiv.style.display = "block";
+// }
 
-function closePopupMenu() { 
-    popupDiv.style.display = "none";
-}
+// function closePopupMenu() { 
+//     popupDiv.style.display = "none";
+// }
 
 // quantity +/- increments
 function up(max) {
@@ -23,7 +23,7 @@ function down(min) {
     }
 }
 
-// modal 
+// modal box
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -32,24 +32,28 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+    if (event.target == modal) {
     modal.style.display = "none";
-  }
+    }
 }
+
+const navBar = document.getElementById('nav-bar');
+const popupMenu = document.getElementById('custom-popup-menu');
 
 function openModal(event) { 
     modal.style.display = "block";
 }
 
+// gets all item class
 const menuItems = document.getElementsByClassName('item');
 // [htmlElement, htmlElement, ...]
-console.log(menuItems);
 
+// adds event listener on click to open modal
 for (const item of menuItems) {
     item.addEventListener('click', (e) => {
         openModal(e);
